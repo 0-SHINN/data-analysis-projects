b286915f4e8a384a1c296be709e9a8d5a01b36fa
0-SHINN <hys3448@naver.com>
2025-12-18 22:42:10 +0900

    Delete project

--- commerce-funnel-analysis/code/time_conversion_rate.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  EXTRACT(HOUR FROM event_time) AS hour,
-  COUNT(DISTINCT CASE WHEN event_type='view' THEN user_id END) AS view_user,
-  COUNT(DISTINCT CASE WHEN event_type='cart' THEN user_id END) AS cart_user,
-  ROUND(COUNT(DISTINCT CASE WHEN event_type='cart' THEN user_id END) / COUNT(DISTINCT CASE WHEN event_type='view' THEN user_id END), 4) AS view_to_cart
-FROM `event_log`
-GROUP BY hour
-ORDER BY view_to_cart DESC
--- commerce-funnel-analysis/code/weekday_conversion_rate.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  EXTRACT(DAYOFWEEK FROM event_time) AS dow,
-  COUNT(DISTINCT CASE WHEN event_type='view' THEN user_id END) AS view_user,
-  COUNT(DISTINCT CASE WHEN event_type='cart' THEN user_id END) AS cart_user,
-  ROUND(COUNT(DISTINCT CASE WHEN event_type='cart' THEN user_id END) / COUNT(DISTINCT CASE WHEN event_type='view' THEN user_id END), 4) AS view_to_cart
-FROM `event_log`
-GROUP BY dow
-ORDER BY view_to_cart DESC
